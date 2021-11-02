4402f45331845679203c547b0b86982a99a53057
Ubuntu <ubuntu@ip-172-31-5-129.ap-northeast-2.compute.internal>
2021-11-02 15:38:14 +0000

    third commit

--- news_app/test.py
+++ news_app/test.py
@@ -1,7 +1,7 @@
-#from django.test import TestCase
 import requests
 from bs4 import BeautifulSoup
-
+import os
+import sys
 
 def create_soup(url):
     res = requests.get(url)
@@ -10,7 +10,20 @@ def create_soup(url):
     return soup
 
 
+def scrape_image_link(link, image_link):
+    url = link
+    #res = requests.get(url, headers={'User-Agent':'Mozilla/5.0'})
+    #res.raise_for_status()
+    #soup = BeautifulSoup(res.text, "lxml")
+    soup = create_soup(url)
+    new_image_link = soup.find("div", attrs={"class" : "ssrcss-ab5fd8-StyledFigureContainer e34k3c21"}).find("img")["src"]
+    image_link.append(new_image_link)
+
+
 def scrape_news():
+    if os.path.exists('bbc.txt'):
+        os.remove('bbc.txt')
+
     print("[news]")
     url = "https://www.bbc.com/news"
     res = requests.get(url, headers={'User-Agent':'Mozilla/5.0'})
@@ -18,14 +31,27 @@ def scrape_news():
     soup = BeautifulSoup(res.text, "lxml")
     soup = create_soup(url)
     news_list = soup.find("ol", attrs={"class" : "gel-layout__item gs-u-float-left@l"}).find_all("li")
+    
+    arr=[]
+    image_link=[]
     for index, news in enumerate(news_list):
         title = news.find("a").get_text().strip()
         link = url.replace('/news', '')+news.find("a")["href"]
-        print("{}. {}".format(index+1, title))
-        print(" (링크) : {})".format(link))
+        #scrape_image_link(link, image_link)
+        arr.append(title+'\n'+link+'\n')
+        #print("{}. {}".format(index+1, title))
+        #print(" (링크) : {})".format(link))
 
+    bbc_fp = open('bbc.txt','w',encoding='utf-8')
+    for i in range(len(arr)):
+        bbc_fp.writelines(arr[i])
+
+    for i in range(len(image_link)):
+        bbc_fp.writelinkes(image_link(i))
+    bbc_fp.close()
     print()
-
 
 if __name__ == "__main__":
     scrape_news()
+
+
--- news_app/views.py
+++ news_app/views.py
@@ -9,13 +9,96 @@ def keyboard(request):
         'type': 'text'
     })
 
+
 @csrf_exempt
 def message(request):
     answer = ((request.body).decode('utf-8'))
     return_json_str = json.loads(answer)
     return_str = return_json_str['userRequest']['utterance']
 
-    if return_str == '테스트':
+    bbc_path = '/home/ubuntu/main/news_app/bbc.txt'
+    bbc_fp = open(bbc_path,'r', encoding='utf-8')
+
+    tmp_str=''
+    for line in bbc_fp.readlines():
+        tmp_str += line
+    tmp_str = tmp_str.strip('\n')
+
+    news_list = list(tmp_str.split('\n'))
+    bbc_fp.close()
+
+    news=''
+    news = "\n".join(news_list)
+    news.rstrip()
+
+    if return_str == 'bbc' or return_str == 'BBC':
+        return JsonResponse({
+            'version': "2.0",
+            'template': {
+                'outputs': [{
+                    "listCard": {
+                        "header": {
+                            "title": "BBC World News Most Read"
+                        },
+                        "items": [
+                            {
+                                "title": news_list[0],
+                                #"description": "",
+                                #"imageUrl": "http://k.kakaocdn.net/dn/APR96/btqqH7zLanY/kD5mIPX7TdD2NAxgP29cC0/1x1.jpg",
+                                "link": {
+                                    "web": news_list[1]
+                                }
+                            },
+                            {
+                                "title": news_list[2],
+                                #"description": "",
+                                #"imageUrl": "http://k.kakaocdn.net/dn/APR96/btqqH7zLanY/kD5mIPX7TdD2NAxgP29cC0/1x1.jpg",
+                                "link": {
+                                    "web": news_list[3]
+                                }
+                            },
+                            {
+                                "title": news_list[4],
+                                #"description": "",
+                                #"imageUrl": "http://k.kakaocdn.net/dn/APR96/btqqH7zLanY/kD5mIPX7TdD2NAxgP29cC0/1x1.jpg",
+                                "link": {
+                                    "web": news_list[5]
+                                }
+                            },
+                            {
+                                "title": news_list[6],
+                                #"description": "",
+                                #"imageUrl": "http://k.kakaocdn.net/dn/APR96/btqqH7zLanY/kD5mIPX7TdD2NAxgP29cC0/1x1.jpg",
+                                "link": {
+                                    "web": news_list[7]
+                                }
+                            },
+                            {
+                                "title": news_list[8],
+                                #"description": "",
+                                #"imageUrl": "http://k.kakaocdn.net/dn/APR96/btqqH7zLanY/kD5mIPX7TdD2NAxgP29cC0/1x1.jpg",
+                                "link": {
+                                    "web": news_list[9]
+                                }
+                            },
+                        ],
+                        "buttons": [
+                            {
+                                "label": "홈페이지",
+                                "action": "webLink",
+                                "webLinkUrl": "https://www.bbc.com/news"
+                            }
+                        ]
+                    }
+                }],
+                'quickReplies': [{
+                    'label': '초기 화면',
+                    'action': 'message',
+                    'messageText': '초기 화면'
+                }]
+            }
+        })
+    if return_str == 'cnn' or return_str == 'CNN':
         return JsonResponse({
             'version': "2.0",
             'template': {
@@ -31,3 +114,4 @@ def message(request):
                 }]
             }
         })
+
