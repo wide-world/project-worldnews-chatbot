f68bf472d428a35f0743be1fda2a33c59cfb39b8
Seyeon Yoon <seyeonlab@gmail.com>
2021-12-04 02:10:33 +0000

    실시간 스크래핑 파일 업로드 및 마무리 작업

---
+++ update/mostread_cli.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+source /home/ubuntu/main/myvenv/bin/activate
+cd /home/ubuntu/main/news_app
+python3 scrapping_cli.py
---
+++ update/summary.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+source /home/ubuntu/main/myvenv/bin/activate
+cd /home/ubuntu/main/news_app/contents
+python3 summary.py
